28c7372167fe1701ab951e55feb7013c13cc3a86
Dekram <madxtreme@gmail.com>
2023-03-19 16:21:59 +0300

    Add fingerings activity.

---
+++ app/src/main/java/com/dekram/trumpetabc/FingeringsActivity.kt
@@ -0,0 +1,11 @@
+package com.dekram.trumpetabc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FingeringsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fingerings)
+    }
+}
