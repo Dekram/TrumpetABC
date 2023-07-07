e5f5138af6ccb475ad19dd070ff82e1701ef07c3
Dekram <madxtreme@gmail.com>
2023-07-07 12:34:18 +0300

    Add viewPager2 with recyclerview.

--- app/src/main/java/com/dekram/trumpetabc/NotesCardActivity.kt
+++ app/src/main/java/com/dekram/trumpetabc/NotesCardActivity.kt
@@ -1,55 +1,64 @@
 package com.dekram.trumpetabc
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class NotesCardActivity : AppCompatActivity() {
 
-    private lateinit var notesCard: ImageView
     private lateinit var playButton: ImageView
     private lateinit var viewPager: ViewPager2
+    private lateinit var tabs: TabLayout
+    private val tabTitles = arrayOf("C", "D", "E", "F", "G", "A", "B")
+    private val notesNames = listOf("C", "D", "E", "F", "G", "A", "B")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes_card)
         viewPager = findViewById(R.id.view_pager)
-        notesCard = findViewById(R.id.notes_card)
-        viewPager.adapter = ViewPagerAdapter()
+        tabs = findViewById(R.id.tabs)
+        viewPager.adapter = PagerAdapter(this, notesNames)
+        TabLayoutMediator(tabs, viewPager) { tabs, position ->
+            tabs.text = tabTitles[position]
+        }.attach()
+    }
+
+}
+
+class PagerAdapter(private val context: Context, private val notesNames: List<String>): RecyclerView.Adapter<PagerAdapter.PageHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PageHolder  =
+        PageHolder(LayoutInflater.from(context).inflate(R.layout.item_card, parent, false))
+
+    override fun onBindViewHolder(holder: PageHolder, position: Int) {
+        holder.notesCard.text = notesNames[position]
+        holder.notesView.setImageResource(
+            when(position) {
+                0 -> R.drawable.ces_dur
+                1 -> R.drawable.b_dur
+                2 -> R.drawable.des_dur
+                3 -> R.drawable.a_dur
+                4 -> R.drawable.b_moll
+                5 -> R.drawable.e_dur
+                6 -> R.drawable.f_dur
+                else -> R.drawable.b_moll
+            }
+        )
+    }
+
+    override fun getItemCount(): Int = notesNames.size
+
+    inner class PageHolder(view: View): RecyclerView.ViewHolder(view){
+        val notesCard: TextView = view.findViewById(R.id.notes_card)
+        val notesView: ImageView = view.findViewById(R.id.notesView)
     }
 }
-
-class ViewPagerAdapter : RecyclerView.Adapter<PagerVH>() {
-
-    private val notes = intArrayOf(
-        R.drawable.a_dur,
-        R.drawable.a_moll,
-        R.drawable.as_dur,
-        R.drawable.b_dur
-    )
-
-    private val sounds = intArrayOf(
-        1,
-        2,
-        3,
-        4
-    )
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerVH =
-        PagerVH(LayoutInflater.from(parent.context).inflate(R.layout.item_card, parent, false))
-
-    override fun onBindViewHolder(holder: PagerVH, position: Int) =
-        holder.itemView.run {
-//            notesCard.setImageResource(notes[position])
-//            playButton.setImageResource(sounds[position])
-        }
-
-    override fun getItemCount(): Int = notes.size
-
-}
-
-class PagerVH(itemView: View) : RecyclerView.ViewHolder(itemView)
