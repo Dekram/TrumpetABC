123b7fcb9a8481096bf3aeaf7ff010b8bf594a98
Dekram <madxtreme@gmail.com>
2023-09-12 13:21:51 +0300

    Change splash screen.

--- app/src/main/java/com/dekram/trumpetabc/SplashScreenActivity.kt
+++ app/src/main/java/com/dekram/trumpetabc/SplashScreenActivity.kt
@@ -1,10 +1,13 @@
 package com.dekram.trumpetabc
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.view.Window
+import android.view.WindowInsets
 import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -14,7 +17,7 @@ class SplashScreenActivity : AppCompatActivity() {
         makeFullScreen()
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
+        Handler(Looper.getMainLooper()).postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
@@ -23,10 +26,14 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private fun makeFullScreen() {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
-        supportActionBar?.hide()
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
